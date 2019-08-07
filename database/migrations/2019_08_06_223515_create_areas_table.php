49d4c6f1b47fa68121311fa7a980ff42ebb3fbd8
ChocheGui4 <chochej10@gmail.com>
2019-08-06 18:05:45 -0700

    Se modificaron sentencias en la tabla areas 6/08/19

--- database/migrations/2019_08_06_223515_create_areas_table.php
+++ database/migrations/2019_08_06_223515_create_areas_table.php
@@ -16,7 +16,7 @@ class CreateAreasTable extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',25);
-            
+
         });
     }
 
