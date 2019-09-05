1a8c16f5ce20f3df267f1e841472a32c1c4a4ec7
ChocheGui4 <chochej10@gmail.com>
2019-09-04 17:57:12 -0700

    Se añadieron los datos de la tabla de productos a la vista de adquirir producto, de tal manera que se utilizan de una manera productiva 4/09/19

--- public/assets/product.js
+++ public/assets/product.js
@@ -1,12 +1,12 @@
 //Función para llamar acciones de un botón automáticamente 
-
-function button(desc, name) {
+/*function button(desc, name) {
     $('#title').text($(name).val());
     $('#namename').val($(name).val());
     $('#desc').val($(desc).val());    
     $('#desc').text($(desc).val());
-}
+}*/
 
+//Método para agregar storage ne la creación de nuevos productos
 function selectedcheck(){
     if ($('#check').prop('checked') ) {
         $('#storage1').prop('disabled',true);
@@ -33,24 +33,7 @@ $('#unitstorage1').on('click', function(){
 $('#type').on('click', function(){
     
     if($('#type').val()=="Sale"){
-        $('#users').empty();
-        $('#time').empty();
-        $('#users').append('<option>15</option>');
-        $('#users').append('<option>100</option>');
-        $('#users').append('<option>200</option>');
-        $('#users').append('<option>300</option>');
-        $('#users').append('<option>400</option>');
-        $('#users').append('<option>500</option>');
-        $('#users').append('<option>600</option>');
-        $('#users').append('<option>700</option>');
-        $('#users').append('<option>800</option>');
-        $('#users').append('<option>900</option>');
-        $('#users').append('<option>1000</option>');
-        $('#time').append('<option>1</option>');
-        $('#time').append('<option>2</option>');
-        $('#time').append('<option>3</option>');
-        $('#time').append('<option>4</option>');
-        $('#time').append('<option>5</option>');
+        alert(ti+" "+per);
         $('#dm').text("years");
     }else{
         $('#users').empty();
@@ -95,6 +78,70 @@ $('#edit7').on('click', function(){
     $('#name').val($('#h37').text());
     $('#description').val($('#sm7').text());    
 });
+
+
+//Mostrar opción de storage
+var ti, per, us, sto, unit;
+function button(desc, name, time, period, users, storage, unitstorage) {
+
+    ti = $(time).val();
+    per = $(period).val();
+    us = $(users).val();
+    sto = $(storage).val();
+    unit = $(unitstorage).val();
+    //alert(ti+" "+per+" "+sto+" "+unit);
+    //Calcular el tiempo 
+    if(per=="years"){
+        $('#time').empty();
+        for (let i = 0; i < ti; i++) {
+            $('#time').append('<option>'+[i+1]+'</option>');
+        }
+        $('#dm').text(per);
+
+    }else{
+        $('#time').empty();
+        for (let i = 0; i < ti; i++) {
+            $('#time').append('<option>'+[i+1]+'</option>');
+        }
+        $('#dm').text(per);
+    }
+    $('#users').empty();
+    for (let i = 1; i < us; i++) {
+        num = i*100;
+        if(num==us){
+            $('#users').append('<option>'+num+'</option>');
+            i=us+1;
+        }else if(num>us){
+            $('#users').append('<option>'+us+'</option>');
+            i=us+1;
+        }else{
+            $('#users').append('<option>'+num+'</option>');
+        }
+    }
+    
+
+    if(sto!=""){
+        $('#storage1').empty();
+        for (let i = 0; i < sto; i++) {
+            $('#storage1').append('<option>'+[i+1]+'</option>');
+        }
+        if(unit=="GB"){
+            $('#storage2').text("GB");
+        }else{
+            $('#storage2').text("TB");
+        }
+        $('#sto').show();
+        
+    }else{
+        $('#sto').hide();
+    }
+    $('#title').text($(name).val());
+    $('#namename').val($(name).val());
+    $('#desc').val($(desc).val());    
+    $('#desc').text($(desc).val());
+}
+
+//Termina mostrar opción de storage
 
 
 
--- resources/views/super/addProduct.blade.php
+++ resources/views/super/addProduct.blade.php
@@ -43,15 +43,16 @@
                                     
                                     <input hidden="hidden" type="textarea" id="name{{ $i }}" name="name" value="{{$product->name}}">
                                     <input hidden="hidden" type="text" id="description{{ $i }}" name="description" value="{{ $product->description }}">
-                                    <input hidden="hidden" type="text" id="t1{{ $i }}" name="t1" value="{{ $product->time }}">
-                                    <input hidden="hidden" type="text" id="p1{{ $i }}" name="p1" value="{{ $product->period }}">
-                                    <input hidden="hidden" type="text" id="s1{{ $i }}" name="s1" value="{{ $product->storage }}">
-                                    <input hidden="hidden" type="text" id="us1{{ $i }}" name="us1" value="{{ $product->unitstorage }}">
+                                    <input type="text" id="t1{{ $i }}" name="t1" value="{{$product->time}}">
+                                    <input type="text" id="p1{{ $i }}" name="p1" value="{{$product->period}}">
+                                    <input type="text" id="user{{ $i }}" name="p1" value="{{$product->users}}">
+                                    <input type="text" id="s1{{ $i }}" name="s1" value="{{$product->storage}}">
+                                    <input type="text" id="us1{{ $i }}" name="us1" value="{{$product->unitstorage}}">
                                     <a
                                         href="#"
                                         id="wdngbutton{{ $i }}"
                                         class="btn btn-primary"
-                                        onClick="button('#description{{$i}}','#name{{$i}}')"
+                                        onClick="button('#description{{$i}}','#name{{$i}}','#t1{{$i}}','#p1{{$i}}','#user{{$i}}','#s1{{$i}}','#us1{{$i}}')"
                                         data-toggle="modal"
                                         data-target="#WDNGmodal"
                                         >Go</a>
@@ -108,7 +109,6 @@
                             </div>
                         </div>
                         <div class="row">
-
                             <div class="col-md-1 col-sm-1 col-xs-1">
                                 <br>
                                 <strong>Type</strong> 
@@ -164,6 +164,25 @@
                                     </select>
 
                                 </div>
+                            </div>
+                        </div>
+                        <div id="sto" class="row">
+                            <div class="col-md-1 col-sm-1 col-xs-1">
+                                <br>
+                                <strong>Storage</strong> 
+                            </div>
+                            <div class="col-lg-2 col-sm-2 col-xs-2">
+                                <br>
+                                <div class="form-group">
+                                    <select id="storage1" class="form-control">
+                                        <option >Nuevo</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-1 col-sm-1 col-xs-1">
+                                <br>
+                                <strong id="storage2" >GB</strong> 
                             </div>
                         </div>
                     </div>
