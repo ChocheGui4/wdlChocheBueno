e549804b7c81e7d5b7472db5fada14152e6390bc
ChocheGui4 <chochej10@gmail.com>
2019-09-09 11:01:07 -0700

    Se guardó un cambio en la vista principal 9/09/19

--- resources/views/super/principal.blade.php
+++ resources/views/super/principal.blade.php
@@ -9,7 +9,7 @@
             <h3 class="text-themecolor m-b-0 m-t-0">Dashboard</h3>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
-                    <a href="javascript:void(0)">Home</a>
+                    <a href="javascript:void(0)">Hom</a>
                 </li>
                 <li class="breadcrumb-item active">Dashboard</li>
             </ol>
