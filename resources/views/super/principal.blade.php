d688dee31e021f8bae93adc2b2bc38e991d0b62d
jose <dsoporte4@warriorslabs.com>
2019-11-05 12:32:42 -0800

    Se dejo el que se va a dejar x3 5/11/19

--- resources/views/super/principal.blade.php
+++ resources/views/super/principal.blade.php
@@ -1,30 +1,6 @@
 @extends('layouts.wdlicenciamiento')
 @section('content')
-<<<<<<< HEAD
-<div class="container-fluid">
-    <!-- ============================================================== -->
-    <!-- Bread crumb and right sidebar toggle -->
-    <!-- ============================================================== -->
-    <div class="row page-titles">
-        <div class="col-md-6 col-8 align-self-center">
-            <h3 class="text-themecolor m-b-0 m-t-0">Dashboard</h3>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="javascript:void(0)">Hom</a>
-                </li>
-                <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-        </div>
-        <div class="col-md-6 col-4 align-self-center">
-            <button
-                class="right-side-toggle waves-effect waves-light btn-info btn-circle btn-sm pull-right m-l-10">
-                <i class="ti-settings text-white"></i>
-            </button>
-            <button class="btn pull-right hidden-sm-down btn-success">
-                <i class="mdi mdi-plus-circle"></i>
-                Create</button>
-            <div class="dropdown pull-right m-r-10 hidden-sm-down">
-=======
+
     <div class="container-fluid">
         <!-- ============================================================== -->
         <!-- Bread crumb and right sidebar toggle -->
@@ -40,7 +16,7 @@
                 </ol>
             </div>
             <div class="col-md-6 col-4 align-self-center">
->>>>>>> f7f0b5b9844dd0ab60f83ca86d37c71bb4f3b40a
+
                 <button
                     class="right-side-toggle waves-effect waves-light btn-info btn-circle btn-sm pull-right m-l-10">
                     <i class="ti-settings text-white"></i>
